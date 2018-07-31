fced707dd32f2c99b1c7482e792351e24a666bc9
IBM <IBM@DEVOPS_12>
2018-07-31 11:28:01 +0530

    bye

--- devops/src/test/java/Runner/Test_Runner1.java
+++ devops/src/test/java/Runner/Test_Runner1.java
@@ -24,7 +24,7 @@ package Runner;
 		  public void beforeTest() throws InterruptedException {
 			  System.setProperty("webdriver.gecko.driver","E:\\gecko\\geckodriver.exe");
 			  driver=new FirefoxDriver();
-			  driver.get("http://ec2-18-222-213-91.us-east-2.compute.amazonaws.com/TestWebapp/");
+			  driver.get("http://ec2-18-222-213-91.us-east-2.compute.amazonaws.com:8090/TestWebapp/");
 			  //sThread.sleep(5000);
 		  }
 
